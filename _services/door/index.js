1f56d1b64622e57d0bbb24d4a1433a2704d6c2a8
Juergen Fitschen <me@jue.yt>
2024-04-26 10:13:58 +0200

    Move to easy-gpiod

--- _services/door/index.js
+++ _services/door/index.js
@@ -1,6 +1,8 @@
-const GPIO = require('onoff').Gpio;
-const door = new GPIO(26, 'in', 'both');
+const {openGpioChip, Input} = require('easy-gpiod');
 
-door.watch((err, opened) => {
-	process.send({ event: 'DOOR', opened });
+const chip = openGpioChip('/dev/gpiochip0');
+const {door} = chip.requestLines('door', {
+	door: Input(26, {bias: 'pull-up', rising_edge: true, falling_edge: true, debounce: 100000})
 });
+
+door.on('change', (opened) => process.send({ event: 'DOOR', opened }));
